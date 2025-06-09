7f84e2c9148f851185d15dd9938d4ac97e75d773
Light - Dang Nguyen Hai <dang.nguyen@orientsoftware.com>
2025-06-09 18:25:44 +0700

    feat(db): generate sqlc files

--- db/queries/transfer.sql
+++ db/queries/transfer.sql
@@ -7,3 +7,15 @@ INSERT INTO transfers (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE from_account_id = $1
+  OR to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
